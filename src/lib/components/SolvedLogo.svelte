ceb851b73611ebb636004438b7e3335876446b94
J-Josu <josue.sa.trabajos@gmail.com>
2022-09-14 13:55:14 -0300

    Updated logo

--- src/lib/components/SolvedLogo.svelte
+++ src/lib/components/SolvedLogo.svelte
@@ -3,24 +3,28 @@
 </script>
 
 <div style="--height:{height};">
-  <span>&lt;s</span>
+  <span class="code">&lt;</span><span class="text">s</span>
   <img src="/argentinian_flag_sun.svg" alt="" />
-  <span>l</span>
-  <span class="inverted">dev</span>
-  <span>/&gt;</span>
+  <span class="text">l</span>
+  <span class="dev">dev</span>
+  <span class="code">/&gt;</span>
 </div>
 
 <style>
   div {
-    font-size: var(--height);
-    text-transform: uppercase;
     display: flex;
     justify-content: center;
+    padding: calc(var(--height) / 4);
     line-height: var(--height);
+    font-size: var(--height);
     font-weight: 900;
     font-family: monospace;
+    text-transform: uppercase;
     --shadow-size: 0px;
-    padding: calc(var(--height) / 4);
+    --dev-color: inherit;
+  }
+  div:hover {
+    --dev-color: hsl(200, 100%, 50%);
   }
   img {
     height: var(--height);
@@ -28,9 +32,19 @@
     filter: drop-shadow(0px 0px var(--shadow-size) hsl(45, 100%, 50%));
   }
   img:hover {
-    --shadow-size: 16px;
+    --shadow-size: calc(var(--height) / 4);
   }
-  .inverted {
+  .dev {
     rotate: y 180deg;
+    color: var(--dev-color);
+    transition: color 0.5s ease-in;
+  }
+  @media screen and (max-width: 688px) {
+    .text {
+      display: none;
+    }
+    .dev {
+      color: hsl(200, 100%, 50%);
+    }
   }
 </style>
